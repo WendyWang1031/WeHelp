6f6c22d9e30b25c8e2464c7d3f73ce40209900db
WendyWang1031 <w5569590@gmail.com>
2024-04-17 11:09:58 +0800

    task3 code

--- week3/task_3/app.js
+++ week3/task_3/app.js
@@ -12,6 +12,14 @@ function listClose() {
   nav.classList.remove("expanded");
 }
 
+function ShortCutString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
 const url =
   "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1";
 
@@ -23,5 +31,47 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
-    console.log(data);
+    let firstImageUrls = data.data.results.map((result) => {
+      let filelistString = result.filelist.toLowerCase();
+      let urls = filelistString.split(".jpg").map((url) => url.trim() + ".jpg");
+      return urls[0];
+    });
+    let locationNames = data.data.results.map((result) => {
+      return result.stitle;
+    });
+
+    let promotionImages = document.querySelectorAll(".promotion img");
+    promotionImages.forEach((img, index) => {
+      if (firstImageUrls[index]) {
+        img.src = firstImageUrls[index];
+      }
+    });
+
+    let promotionTitle = document.querySelectorAll(".promotion > div > div");
+    promotionTitle.forEach((title, index) => {
+      if (locationNames[index]) {
+        title.textContent = ShortCutString(locationNames[index], 7);
+      }
+    });
+
+    let titleDivsImage = document.querySelectorAll(".title1-area > div");
+    titleDivsImage.forEach((div, index) => {
+      if (firstImageUrls[index + promotionImages.length]) {
+        div.style.backgroundImage = `url(${
+          firstImageUrls[index + promotionImages.length]
+        })`;
+      }
+    });
+
+    let titleDivsTitle = document.querySelectorAll(
+      ".title1-area > div > span > p "
+    );
+    titleDivsTitle.forEach((title, index) => {
+      if (locationNames[index + promotionTitle.length]) {
+        title.textContent = ShortCutString(
+          locationNames[index + promotionTitle.length],
+          7
+        );
+      }
+    });
   });
