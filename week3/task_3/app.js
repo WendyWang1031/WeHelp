e5be2c8496f037ce4cf9a4fd413c9bf445633687
WendyWang1031 <w5569590@gmail.com>
2024-04-17 15:09:44 +0800

    update task3

--- week3/task_3/app.js
+++ week3/task_3/app.js
@@ -1,6 +1,8 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const nav = document.querySelector(".navbar-nav");
 const closeBtn = document.querySelector(".close-btn");
+const loadMoreBtn = document.getElementById("loadMoreBtn");
+const btnArea = document.getElementById("btn-area");
 
 mobileMenu.addEventListener("click", listExpended);
 function listExpended() {
@@ -12,13 +14,11 @@ function listClose() {
   nav.classList.remove("expanded");
 }
 
-function ShortCutString(str, num) {
-  if (str.length > num) {
-    return str.slice(0, num) + "...";
-  } else {
-    return str;
-  }
-}
+loadMoreBtn.addEventListener("click", loadMoreContent);
+
+let allImageUrls = [];
+let allLocationNames = [];
+let currentIndex = 0;
 
 const url =
   "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1";
@@ -31,34 +31,34 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
-    let firstImageUrls = data.data.results.map((result) => {
+    allImageUrls = data.data.results.map((result) => {
       let filelistString = result.filelist.toLowerCase();
       let urls = filelistString.split(".jpg").map((url) => url.trim() + ".jpg");
       return urls[0];
     });
-    let locationNames = data.data.results.map((result) => {
+    allLocationNames = data.data.results.map((result) => {
       return result.stitle;
     });
 
     let promotionImages = document.querySelectorAll(".promotion img");
     promotionImages.forEach((img, index) => {
-      if (firstImageUrls[index]) {
-        img.src = firstImageUrls[index];
+      if (allImageUrls[index]) {
+        img.src = allImageUrls[index];
       }
     });
 
     let promotionTitle = document.querySelectorAll(".promotion > div > div");
     promotionTitle.forEach((title, index) => {
-      if (locationNames[index]) {
-        title.textContent = ShortCutString(locationNames[index], 7);
+      if (allLocationNames[index]) {
+        title.textContent = allLocationNames[index];
       }
     });
 
     let titleDivsImage = document.querySelectorAll(".title1-area > div");
     titleDivsImage.forEach((div, index) => {
-      if (firstImageUrls[index + promotionImages.length]) {
+      if (allImageUrls[index + promotionImages.length]) {
         div.style.backgroundImage = `url(${
-          firstImageUrls[index + promotionImages.length]
+          allImageUrls[index + promotionImages.length]
         })`;
       }
     });
@@ -67,11 +67,52 @@ fetch(url)
       ".title1-area > div > span > p "
     );
     titleDivsTitle.forEach((title, index) => {
-      if (locationNames[index + promotionTitle.length]) {
-        title.textContent = ShortCutString(
-          locationNames[index + promotionTitle.length],
-          7
-        );
+      if (allLocationNames[index + promotionTitle.length]) {
+        title.textContent = allLocationNames[index + promotionTitle.length];
       }
     });
   });
+
+function loadMoreContent() {
+  const original = document.querySelector(".whole-main-title1-area");
+  const clone = original.cloneNode(true);
+  btnArea.parentNode.insertBefore(clone, btnArea);
+
+  const clonedImages = clone.querySelectorAll(".title1-area > div");
+  const clonedTitles = clone.querySelectorAll(
+    ".promotion > div > div, .title1-area > div > span > p"
+  );
+  const allCurrentImages = document.querySelectorAll(".title1-area > div");
+  const allCurrentTitles = document.querySelectorAll(
+    ".whole-main-title1-area .promotion > div > div, .whole-main-title1-area .title1-area > div > span > p"
+  );
+  const newStartIndex =
+    allCurrentImages.length -
+    clone.querySelectorAll(".title1-area > div").length;
+
+  clonedImages.forEach((div, index) => {
+    const imageIndex = newStartIndex + index;
+    if (imageIndex < allImageUrls.length) {
+      div.style.backgroundImage = `url(${allImageUrls[imageIndex]})`;
+    }
+  });
+  clonedTitles.forEach((title, index) => {
+    const titleIndex = newStartIndex + index;
+    if (titleIndex < allLocationNames.length) {
+      title.textContent = allLocationNames[titleIndex];
+    }
+  });
+
+  currentIndex += newStartIndex + clonedImages.length + clonedTitles.length;
+}
+
+function shortCutString(element, num, maxWidth) {
+  const textContent = element.textContent;
+  const elementWidth = element.clientWidth;
+
+  if (elementWidth < maxWidth && textContent > num) {
+    return textContent.slice(0, num) + "...";
+  } else {
+    return textContent;
+  }
+}
