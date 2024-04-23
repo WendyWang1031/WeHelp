d0d4b45e6918ade036acff51ca3200a163efa948
WendyWang1031 <w5569590@gmail.com>
2024-04-23 16:25:28 +0800

    task4 issue

--- week4/main.py
+++ week4/main.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI , Request , Form , HTTPException , status , Query
 from fastapi.responses import HTMLResponse , RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-# from starlette.applications import Starlette
-# from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
 
 
@@ -14,13 +12,6 @@ app = FastAPI()
 app.mount("/static" , StaticFiles ( directory = "static" ) , name = "static")
 app.add_middleware(SessionMiddleware , secret_key = "your-secret-key")
 templates = Jinja2Templates( directory = "templates" )
-
-
-# @app.get("/index/{id}" , response_class= HTMLResponse )
-# async def read_item(request: Request, id: str):
-#     return templates.TemplateResponse(
-#         request = request , name = "index.html" , context = {"id" : id}
-#     )
 
 
 @app.get("/" , response_class= HTMLResponse )
@@ -38,8 +29,8 @@ async def get_signout(request: Request):
 
 
 @app.post("/signin/")
-async def signin(request : Request , username :  str = Form(default = ""), password :  str = Form(default = "")):
-    print("See signin successful or not?") 
+async def signin(request : Request , username :  str = Form(default = "") , password :  str = Form(default = "")  ):
+    
     if username == "test" and password == "test":
         request.session["SIGNED-IN"] = True
         response = RedirectResponse(url="/member" , status_code= status.HTTP_302_FOUND)
@@ -53,6 +44,8 @@ async def signin(request : Request , username :  str = Form(default = ""), passw
         response = RedirectResponse(url = f"/error?message={quote(error_message)}" , status_code = status.HTTP_302_FOUND)
         return response
 
+    
+
 @app.get("/member/" , response_class = HTMLResponse )
 async def signin_successed(request: Request):
     if "SIGNED-IN" in request.session and request.session["SIGNED-IN"]:
@@ -65,3 +58,14 @@ async def signin_successed(request: Request):
 @app.get("/error" , response_class = HTMLResponse )
 async def show_error(request : Request , message : str = ""):
     return templates.TemplateResponse("error.html" , {"request" : request , "error" : message})
+
+@app.get("/square/{cal}" , response_class = HTMLResponse )
+async def square_math(request : Request , cal : int):
+    result = cal * cal
+    return templates.TemplateResponse("square.html" , {"request" : request , "result" : result})
+
+# @app.post("/calculate/")
+# async def calculate_square( number : int = Form(default = 0)):
+    
+#     response = RedirectResponse(url=f"/square/{number}" , status_code= status.HTTP_302_FOUND)
+#     return response
--- week4/static/js/app.js
+++ week4/static/js/app.js
@@ -1,8 +1,12 @@
 const submitBtn = document.querySelector(".submit-btn");
 const agreeCheckBox = document.getElementById("agree-box");
+const calBtn = document.querySelector(".cal-btn");
 
 submitBtn.addEventListener("click", checkUserHitBox);
 agreeCheckBox.addEventListener("click", checkUserAgree);
+document.addEventListener("DOMContentLoaded", (event) => {
+  calBtn.addEventListener("click", checkCalculate);
+});
 
 function checkUserAgree() {
   if (agreeCheckBox.checked) {
@@ -20,4 +24,16 @@ function checkUserHitBox() {
   } else {
     console.log("good job! You agreed!");
   }
+}
+
+function checkCalculate(event) {
+  const inputNumber = document.getElementById("number").value;
+  let number = parseInt(inputNumber);
+  if (number < 0 || isNaN(number)) {
+    alert("Please enter a positive number");
+    event.preventDefault();
+    return;
+  }
+
+  window.location.href = `/square/${number}`;
 }
