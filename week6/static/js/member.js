b54319e33c93f20c57411ee403993509b4e1fe11
WendyWang1031 <w5569590@gmail.com>
2024-05-08 12:39:40 +0800

    week 6 task6 front-end interface delete btn func

--- week6/static/js/member.js
+++ week6/static/js/member.js
@@ -1,6 +1,16 @@
 const messageSubmitBtn = document.querySelector(".submit-message-btn");
+const deleteMeassgeBtn = document.querySelector(".delete-message-btn");
+const btnContainer = document.querySelector(".for-center");
 
 messageSubmitBtn.addEventListener("click", checkMessage);
+
+if (btnContainer) {
+  btnContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete-message-btn")) {
+      checkDeleteMessage(event);
+    }
+  });
+}
 
 function checkMessage(event) {
   const messageInput = document.querySelector("#message_content").value;
@@ -10,5 +20,14 @@ function checkMessage(event) {
     return;
   } else {
     console.log("good job! Message field are filled!");
+  }
+}
+
+function checkDeleteMessage(event) {
+  event.preventDefault();
+  if (confirm("Are you sure you want to delete the message?") == true) {
+    console.log("Yes,delete");
+  } else {
+    console.log("Nooooo!");
   }
 }
